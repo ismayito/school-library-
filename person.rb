a1984ef6e4393354613052437c5c3555b8d4d833
Ismail mayito <ismayito1@gmail.com>
2023-11-15 13:18:18 +0300

    Fix linter errors

--- person.rb
+++ person.rb
@@ -11,6 +11,7 @@ class Decorator < Nameable
 
   def initialize(nameable)
     @nameable = nameable
+    super()
   end
 
   def correct_name
@@ -38,6 +39,7 @@ end
 
 class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
