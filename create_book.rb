88b1d548cc561431be2ef182ef9c95fc25831b6f
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-22 15:00:22 +0300

    Save books to json file

--- create_book.rb
+++ create_book.rb
@@ -1,5 +1,7 @@
 require './app'
 require './book'
+require 'json'
+
 class CreateBook
   def initialize(books, app_instance)
     @books = books
@@ -12,11 +14,36 @@ class CreateBook
     book_name = gets.chomp
     puts 'Enter book Author:'
     book_author = gets.chomp
+
+    # Load existing books from the JSON file
+    existing_books = load_books_from_json
+
     added_book = Book.new(book_name, book_author)
     @books.push(added_book)
+    existing_books.push({ title: added_book.title, author: added_book.author })
+
+    save_books_to_json(existing_books)
+
     puts 'Book successfully created'
     puts 'Choose another option to perform another operation'
     @app_instance.options_list
     @app_instance.choose_option
+  end
+
+  # method to save books to 'books.json'
+  def save_books_to_json(books)
+    File.open('books.json', 'w') do |file|
+      file.puts JSON.pretty_generate(books)
+    end
+  end
+
+  # method to load books from 'books.json'
+  def load_books_from_json
+    if File.exist?('books.json')
+      json_data = File.read('books.json')
+      JSON.parse(json_data)
+    else
+      []
+    end
   end
 end
