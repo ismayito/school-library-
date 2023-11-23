d45f24f7eff84f5db6e88a77bcd23ee0d9f6e787
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-23 04:44:29 +0300

    Preserve books data to json

--- book.rb
+++ book.rb
@@ -11,4 +11,12 @@ class Book
   def add_rental(person, date)
     Rental.new(date, person, self)
   end
+
+  def to_json(*args)
+    {
+      title: @title,
+      author: @author,
+      rentals: @rentals
+    }.to_json(*args)
+  end
 end
