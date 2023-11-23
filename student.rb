4c9017071c60890198b3813d10289be1f36e7f94
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-23 04:46:12 +0300

    Add people data to json

--- student.rb
+++ student.rb
@@ -17,4 +17,16 @@ class Student < Person
   def play_hooky
     '¯(ツ)/¯'
   end
+
+  def to_json(*args)
+    {
+      class: self.class,
+      id: @id,
+      name: @name,
+      age: @age,
+      parent_permission: @parent_permission,
+      rentals: @rentals,
+      classroom: @classroom
+    }.to_json(*args)
+  end
 end
