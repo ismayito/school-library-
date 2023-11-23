0de614895620c40c4b04976bc3146a92f1667d08
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-23 04:44:59 +0300

    Preserve rental data to json

--- create_rental.rb
+++ create_rental.rb
@@ -1,40 +1,87 @@
+require './app'
 require './book'
 require './student'
 require './teacher'
 require './rental'
 
-def create_rental
-  list_person
+def create_rental(app_instance)
+  list_person(app_instance)
   print 'ID of person: (Choose "ID" from the above list)'
   person_id = gets.chomp.to_i
-  person = @person.find { |p| p.id == person_id }
-  list_book
-  print 'book type: (Choose book from the above list)'
+
+  # Find the person, whether it's an instance of Person or a hash
+  person = app_instance.person.find do |p|
+    if p.is_a?(Person)
+      p.id == person_id
+    else
+      p['id'] == person_id
+    end
+  end
+
+  # Use a more explicit condition to check if person is found
+  if person
+    puts "Found person: #{person.inspect}"
+  else
+    puts 'Error: Person not found.'
+    return # Exit the method if person is not found
+  end
+
+  list_book(app_instance)
+  print 'book title: (Choose book from the above list)'
   book_title = gets.chomp
-  book = @books.find { |b| b.title == book_title }
+
+  # Find the book, whether it's an instance of Book or a hash
+  book = app_instance.books.find do |b|
+    if b.is_a?(Book)
+      b.title == book_title
+    else
+      b['title'] == book_title
+    end
+  end
+
   print 'Enter rental date: '
   rental_date = gets.chomp
-  rental = Rental.new(rental_date, person, book)
-  @rentals << rental
-  puts 'Rental created successfully.'
+
+  if person.is_a?(Hash)
+    rental = Rental.new(rental_date, person, book)
+    person['rentals'] << rental
+    app_instance.rentals << rental
+    puts 'Rental created successfully.'
+  else
+    puts 'Error: Person not found.'
+  end
+  app_instance.options_list
+  app_instance.choose_option
 end
 
-def list_person
-  if @person.empty?
+def list_person(app_instance)
+  if app_instance.person.empty?
     puts 'No people added yet. Enter 3 to create a person.'
-    choose_option
+    app_instance.choose_option
   else
     puts 'List of all available people:'
-    @person.each { |person| puts "[ID: #{person.id}] Name: #{person.name} Age: #{person.age} Class: #{person.class}" }
+    app_instance.person.each do |person|
+      if person.is_a?(Person)
+        puts "[ID: #{person.id.to_i}] Name: #{person.name} Age: #{person.age} Class: #{person.class}"
+      else
+        puts "[ID: #{person['id'].to_i}] Name: #{person['name']} Age: #{person['age']} Class: #{person['class']}"
+      end
+    end
   end
 end
 
-def list_book
-  if @books.empty?
+def list_book(app_instance)
+  if app_instance.books.empty?
     puts 'No books added yet. Enter 4 to create a new book.'
-    choose_option
+    app_instance.choose_option
   else
     puts 'List of all available books:'
-    @books.each { |book| puts "Book: #{book.title} Author: #{book.author}" }
+    app_instance.books.each do |book|
+      if book.is_a?(Book)
+        puts "Book: #{book.title} Author: #{book.author}"
+      else
+        puts "Book: #{book['title']} Author: #{book['author']}"
+      end
+    end
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,12 +1,17 @@
 require './person'
 require './book'
+
 class Rental
   attr_accessor :date, :person, :book
 
   def initialize(date, person, book)
     @date = date
-    @person = person
-    person.rentals << self
+    if person.is_a?(Person)
+      @person = person
+      person.rentals << self
+    end
+    return unless book.is_a?(Book)
+
     @book = book
     book.rentals << self
   end
@@ -19,9 +24,27 @@ class Rental
   def persons=(person)
     person.rentals.push(person) unless person.rentals.include?(person)
   end
-end
 
-person = Person.new(22, 'mayito')
-book = Book.new('comic', 'author')
-rent = Rental.new('12-02-2023', person, book)
-p rent
+  def to_json(*args)
+    {
+      person_id: @person&.id, # Use safe navigation operator &. to avoid calling id on nil
+      book_title: @book&.title,
+      book_author: @book&.author,
+      date: @date
+    }.to_json(*args)
+  end
+
+  def self.load_rentals_from_json(persons, books)
+    if File.exist?('rentals.json') && !File.empty?('rentals.json')
+      json_data = File.read('rentals.json')
+      json_array = JSON.parse(json_data)
+      json_array.map do |rental_data|
+        person = persons.find { |p| p.id == rental_data['person_id'] }
+        book = books.find { |b| b.title == rental_data['book_title'] && b.author == rental_data['book_author'] }
+        Rental.new(rental_data['date'], person, book)
+      end
+    else
+      []
+    end
+  end
+end
