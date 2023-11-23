908cc912f98c543e9316df9bcca23d713a3d6607
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-23 22:32:06 +0300

    Alter id to offer either entry or randomization

--- person.rb
+++ person.rb
@@ -3,9 +3,9 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', id: nil, parent_permission: true)
     super()
-    @id = Random.rand(1..1000)
+    @id = id || Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
